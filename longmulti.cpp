a530609ec40608d4a60ff9b93152ae51d4d91ff3
roiflint <65483806+roiflint@users.noreply.github.com>
2020-12-01 16:14:37 +0200

    fixed longmulti and print

--- longmulti.cpp
+++ longmulti.cpp
@@ -2,7 +2,10 @@
 
 int* longMulti(int* a, int* b, int n) {
     int* res = new int[2*n];
-
+    for (int t = 0; t < 2n; t++)
+    {
+        res[t] = 0;
+    }
     for(int i = n-1; i >= 0; i--) {
         for (int j = n-1; j >= 0; j--) {
             res[i+j+1] += (a[i] * b[j]) % 10;
--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ int main() {
 
     /* longMulti test: */
     
-    // int n = 4;
-    // int a[4] = {2, 2, 2, 2};
-    // int b[4] = {0, 0, 5, 3};
+     int n = 4;
+     int a[4] = {9, 9, 9, 9};
+     int b[4] = {0, 0, 5, 3};
 
-    // int* res = longMulti(a, b, n);
+     int* res = longMulti(a, b, n);
+     int j = 0;
+     while (res[j]==0)
+     {
+         j++;
+     }
+     for (int i = j; i < 2 * n; i++) {
+         cout << res[i];
+     }
 
-    // for (int i = 0; i < 2 * n; i++) {
-    //     cout << res[i];
-    // }
-
-    // cout << endl;
+     cout << endl;
 
     return 0;
 }
