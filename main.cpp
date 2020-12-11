573c3361bf1da6650176dd2929a124e18522b577
roiflint <65483806+roiflint@users.noreply.github.com>
2020-12-11 13:49:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,29 @@ using namespace std;
 int main() {
 
     /* longMulti test: */
-    int n = 3;
+    int n = 4;
     int *a = new int[n];
     int *b = new int[n];
     a[0] = 1;
     a[1] = 2;
     a[2] = 3;
+    a[3] = 4;
     b[0] = 2;
     b[1] = 2;
     b[2] = 5;
+    b[3] = 9;
     int* res = new int[2 * n];
     for (int i = 0; i < 2 * n; i++)
     {
         res[i] = 0;
     }
-    karatsubaRec(a, b, n, res);
+    karatsubaRec(a, b, n, &res);
+    if (n%2!=0)
+    {
+        n++;
+    }
     int j = 0;
-    while (res[j] == 0)
+    while (res[j] == 0 && j<n)
     {
         j++;
     }
