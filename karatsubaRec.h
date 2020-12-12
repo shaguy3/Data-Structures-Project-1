1668a83061b995b4af4bf277f9130f6786d01eca
roiflint <65483806+roiflint@users.noreply.github.com>
2020-12-12 20:12:38 +0200

    Update karatsubaRec.h

--- karatsubaRec.h
+++ karatsubaRec.h
@@ -3,10 +3,10 @@
 
 void karatsubaRec(int* a, int* b, int n, int** c);
 
-int* sum(int* a, int* b, int n);
+int* sum(int* a, int* b, int mid);
 void subtract(int** a, int* b, int n);
 void addZeroes(int** a, int n);
-void shiftLeft(int* a, int* c, int n, int shift);
+void shiftLeft(int* a, int* c, int na,int nc, int shift);
 void fixDigits(int* c, int n);
 void makeEqual(int** a, int n, int size);
 int removeZeroes(int** a, int** b, int n);
