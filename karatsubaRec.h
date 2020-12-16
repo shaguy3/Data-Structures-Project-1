c22da0dc6ba9a78758a11e1776938296dc6316ed
shaguy3 <44630692+shaguy3@users.noreply.github.com>
2020-12-16 09:16:12 +0200

    Update karatsubaRec.h

--- karatsubaRec.h
+++ karatsubaRec.h
@@ -1,11 +1,10 @@
 #pragma once
 
-
-void karatsubaRec(int* a, int* b, int n, int** c);
+void karatsubaRec(int* a, int* b, int n, int** res);
 
 int* sum(int* a, int* b, int mid);
 void subtract(int** a, int* b, int na, int nb);
 void addZeroes(int** a, int n);
-void shiftLeft(int* a, int* c, int na,int nc, int shift);
+void shiftLeft(int* a, int* c, int na, int nc, int shift);
 void fixDigits(int* c, int n);
 int removeZeroes(int** a, int** b, int n);
