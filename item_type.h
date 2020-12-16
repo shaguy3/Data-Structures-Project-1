4c74b719deb140c4ed31f250a72d7941e11480b3
shaguy3 <44630692+shaguy3@users.noreply.github.com>
2020-12-16 09:23:04 +0200

    Update item_type.h

--- item_type.h
+++ item_type.h
@@ -1,8 +1,12 @@
+#pragma once
+
 class ItemType {
-    public:
-        int var;
-        ItemType* next;
-        ItemType();
-        ItemType(int var);
-        ~ItemType();
+public:
+    int* x, * y, * res, * p1, * p2, * p3;
+    int n, p1_n, p2_n, p3_n, count, line;
+    ItemType* next;
+
+    ItemType(int* _x, int* _y, int* _res, int _n, int _line, \
+        int* _p1, int _p1_n, int* _p2, int _p2_n, int* p3, int p3_n, int count);
+    ~ItemType();
 };
