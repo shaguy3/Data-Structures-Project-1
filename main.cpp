7ead35282dde46b09036f919f6488f0348f12a77
shaguy3 <44630692+shaguy3@users.noreply.github.com>
2020-12-16 09:11:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,173 @@
 #include <iostream>
+#include "longmulti.h"
 #include "karatsubaRec.h"
+#include "karatsubaIter.h"
+#include "stack.h"
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+#include <fstream>
+#include <string>
+
 using namespace std;
+
+bool check_input(string& a, string& b, string& n) {
+
+    while (n[0] == ' ' || n[0] == '0') {
+        n.erase(0, 1);
+    }
+
+    for (int i = 0; i < n.length(); i++) {
+        if (!isdigit(n[i])) {
+            return false;
+        }
+    }
+
+    int num_n = atoi(n.c_str());
+
+    while (a[0] == ' ') {
+        a.erase(0, 1);
+    }
+
+    while (b[0] == ' ') {
+        b.erase(0, 1);
+    }
+
+    if (a.length() != num_n || b.length() != num_n) {
+        return false;
+    }
+
+    for (int i = 0; i < a.length(); i++) {
+        if (!isdigit(a[i]) || (!isdigit(b[i]))) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
 
-    /* longMulti test: */
-    int n = 9;
-    int *a = new int[n]; 
-    int *b = new int[n];
-   //      0 
+    string str_n;
+    getline(cin, str_n);
+    string str_a;
+    getline(cin, str_a);
+    string str_b;
+    getline(cin, str_b);
 
-    a[0] = 3;
-    a[1] = 2;
-    a[2] = 3;
-    a[3] = 2;
-    a[4] = 3;
-    a[5] = 2;
-    a[6] = 3;
-    a[7] = 2;
-    a[8] = 3;
+    if (check_input(str_a, str_b, str_n) && str_n.length() != 0) {
 
-    b[0] = 2;
-    b[1] = 3;
-    b[2] = 2;
-    b[3] = 3;
-    b[4] = 2;
-    b[5] = 3;
-    b[6] = 2;
-    b[7] = 3;
-    b[8] = 2;
+        int n = atoi(str_n.c_str());
+        int* a = new int[n];
+        int* b = new int[n];
 
-    int* res = new int[2 * n];
-    for (int i = 0; i < 2 * n; i++)
-    {
-        res[i] = 0;
-    }
-    karatsubaRec(a, b, n, &res);
-    if (n%2!=0)
-    {
-        n++;
-    }
-    int j = 0;
-    while (res[j] == 0 && j<n)
-    {
-        j++;
-    }
-    for (int i = j; i < 2*n; i++) {
-        cout << res[i] << " ";
-    }
+        for (int i = 0; i < n; i++) {
+            a[i] = (int)(str_a[i] - '0');
+            b[i] = (int)(str_b[i] - '0');
+        }
 
-    cout << endl;
+        int* res_1 = new int[2 * n];
+        int* res_2 = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++)
+        {
+            res_1[i] = 0;
+            res_2[i] = 0;
+        }
+
+        auto start = chrono::high_resolution_clock::now();
+        // unsync the I/O of C and C++.
+        ios_base::sync_with_stdio(false);
+        int* long_res = longMulti(a, b, n);// Here you put the name of the function you wish to measure
+        auto end = chrono::high_resolution_clock::now();
+        // Calculating total time taken by the program.
+        double time_taken =
+            chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+        time_taken *= 1e-9;
+        ofstream myfile("Measure.txt"); // The name of the file
+        myfile << "Time taken by function longMulti is : " << fixed
+            << time_taken << setprecision(9);
+        myfile << " sec" << endl;
+
+        start = chrono::high_resolution_clock::now();
+        // unsync the I/O of C and C++.
+        ios_base::sync_with_stdio(false);
+        karatsubaRec(a, b, n, &res_1);// Here you put the name of the function you wish to measure
+        end = chrono::high_resolution_clock::now();
+        // Calculating total time taken by the program.
+        time_taken =
+            chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+        time_taken *= 1e-9;
+        myfile << "Time taken by function karatsubaRec is : " << fixed
+            << time_taken << setprecision(9);
+        myfile << " sec" << endl;
+
+        start = chrono::high_resolution_clock::now();
+        // unsync the I/O of C and C++.
+        ios_base::sync_with_stdio(false);
+        karatsubaIter(a, b, n, res_2);// Here you put the name of the function you wish to measure
+        end = chrono::high_resolution_clock::now();
+        // Calculating total time taken by the program.
+        time_taken =
+            chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+        time_taken *= 1e-9;
+        myfile << "Time taken by function karatsubaIter is : " << fixed
+            << time_taken << setprecision(9);
+        myfile << " sec" << endl;
+        myfile.close();
+
+        int j = 0;
+        while (long_res[j] == 0 && j <  2 * n) {
+            j++;
+        }
+        for (int i = j; i < 2 * n; i++) {
+            cout << long_res[i];
+        }
+
+        if (j == 2 * n) { cout << "0"; }
+
+        cout << endl;
+
+        if (n % 2 != 0)
+        {
+            n++;
+        }
+
+        j = 0;
+        while (res_1[j] == 0 && j < 2 * n)
+        {
+            j++;
+        }
+        for (int i = j; i < 2 * n; i++) {
+            cout << res_1[i];
+        }
+
+        if (j == 2 * n) { cout << "0"; }
+
+        cout << endl;
+
+        j = 0;
+        while (res_2[j] == 0 && j < 2 * n)
+        {
+            j++;
+        }
+        for (int i = j; i < 2 * n; i++) {
+            cout << res_2[i];
+        }
+
+        if (j == 2 * n) { cout << "0"; }
+
+        cout << endl;
+
+        delete[] a;
+        delete[] b;
+        delete[] res_1;
+        delete[] res_2;
+        delete[] long_res;
+
+    }
+    else {
+        cout << "wrong output" << endl;
+    }
 
     return 0;
 }
