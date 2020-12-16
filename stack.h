0e00b6ad459c5eb6854618cd719292805a99543b
shaguy3 <44630692+shaguy3@users.noreply.github.com>
2020-12-16 09:12:08 +0200

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,14 +1,16 @@
+#pragma once
 #include "item_type.h"
 
+
 class Stack {
-    private:
-        ItemType* head;
-    public:
-        /* Constructors and destructors */
-        Stack();
-        ~Stack();
-        /* Stack methods */
-        bool isEmpty();
-        void push(ItemType* item);
-        ItemType* pop();
+private:
+    ItemType* head;
+public:
+    /* Constructors and destructors */
+    Stack();
+    ~Stack();
+    /* Stack methods */
+    bool isEmpty();
+    void push(ItemType* item);
+    ItemType* pop();
 };
