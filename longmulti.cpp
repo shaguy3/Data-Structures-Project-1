bc9e0a90a8057f6a0d24ec33f8bd11147db57c81
shaguy3 <44630692+shaguy3@users.noreply.github.com>
2020-12-16 09:12:33 +0200

    Update longmulti.cpp

--- longmulti.cpp
+++ longmulti.cpp
@@ -1,17 +1,20 @@
 #include "longmulti.h"
+#include "karatsubaRec.h"
 
 int* longMulti(int* a, int* b, int n) {
-    int* res = new int[2*n];
-    for (int t = 0; t < 2n; t++)
+    int* res = new int[2 * n];
+    for (int t = 0; t < 2 * n; t++)
     {
         res[t] = 0;
     }
-    for(int i = n-1; i >= 0; i--) {
-        for (int j = n-1; j >= 0; j--) {
-            res[i+j+1] += (a[i] * b[j]) % 10;
-            res[i+j] += (a[i] * b[j]) / 10;
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = n - 1; j >= 0; j--) {
+            res[i + j + 1] += (a[i] * b[j]) % 10;
+            res[i + j] += (a[i] * b[j]) / 10;
         }
     }
 
-    return res; 
+    fixDigits(res, 2 * n);
+
+    return res;
 }
