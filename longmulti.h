fa0d5bab0deec2da0bf422245853dc77bce75c7d
shaguy3 <44630692+shaguy3@users.noreply.github.com>
2020-12-16 09:21:09 +0200

    Update longmulti.h

--- longmulti.h
+++ longmulti.h
@@ -1,8 +1,10 @@
+#pragma once
+#include "karatsubaRec.h"
 
 /* longMulti:
  * Input: Two same sized integer arrays (a,b) which contain long numbers,
  *        and the size of the arrays (n).
- * 
+ *
  * Output: An array of size 2n which contains the long multiplication result of a and b.
  */
 int* longMulti(int* a, int* b, int n);
